e3805793436067af923d5c32db254560ecf36867
areebabashir <areebabashir333@gmail.com>
2024-07-24 21:37:59 -0700

    first commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,9 @@ import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
     try {
+
         const { name, email, password, phone, address, answer } = req.body;
+    console.log(req.body);
         //validations
         if (!name) {
             return res.send({ error: "Name is Required" });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
-
 import express from 'express';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import formRoutes from './routes/formRoute.js';
 import authRoutes from './routes/authRoute.js';
 import eventRoutes from './routes/eventRoute.js';
@@ -12,11 +12,9 @@ import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 // Define __dirname for ES Module context
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 // Database configuration
 connectDB();
@@ -24,9 +22,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Add this line to parse JSON bodies
 app.use(cors());
-
+app.use(express.json()); // This line is redundant with bodyParser.json(), but it's okay to keep
 
 // Serve static files from the 'uploads' directory
 app.use('/upload', express.static(path.join(__dirname, 'uploads')));
@@ -36,13 +35,10 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/form', formRoutes);
 app.use('/api/v1/event', eventRoutes);
 
-
 // Test API endpoint
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to ecommerce app</h1>');
 });
-
-
 
 const PORT = process.env.PORT || 8080;
 
